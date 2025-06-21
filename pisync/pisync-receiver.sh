4e1ba7244b7181b74640fc01757c31a24de5e9f0
kopper42 <kopper42@gmail.com>
2025-06-21 14:00:51 -0600

    If the source file doesn't exist, don't copy it.

--- pisync/pisync-receiver.sh
+++ pisync/pisync-receiver.sh
@@ -4,4 +4,7 @@
 DEST_PATH="/etc/pihole/hosts/custom.list"
 SOURCE_PATH="/opt/pisync/custom.list"
 
-cp -f $SOURCE_PATH $DEST_PATH
+
+if [ -f $SOURCE_PATH ]; then
+    cp -f $SOURCE_PATH $DEST_PATH
+fi
