0ffd917f6eb400a8a2d036bf86164255bb73f00f
kopper42 <kopper42@gmail.com>
2025-06-21 14:06:48 -0600

    Adjusted pisync-ssh-push.sh to use the correct path for the custom.list file.

--- pisync/pisync-ssh-push.sh
+++ pisync/pisync-ssh-push.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-## sudo useradd -r -s /sbin/nologin -c "PiSync Service User" -m -d /opt/pisync pisync
-# sudo useradd -r -c "PiSync Service User" -m pisync
-# ssh-keygen -t rsa -b 4096 -C "PiSync Service" -N "" -f /home/pisync/.ssh/id_rsa_service
-
 
 LOCAL_FILE="/etc/pihole/hosts/custom.list"
-SCP_SERVER="192.168.1.250"
-SCP_PATH="/home/pisync/custom.list"
+SCP_SERVER="172.16.0.250"
+SCP_PATH="/opt/pisync/custom.list"
 SCP_USER="pisync"
-SCP_CERT="/home/pisync/.ssh/id_rsa_service"
+SCP_CERT="/opt/pisync/.ssh/id_rsa_service"
 
 
 scp -q -i $SCP_CERT $LOCAL_FILE $SCP_USER@$SCP_SERVER:$SCP_PATH
