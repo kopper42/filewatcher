16fb98b9e4d1f540f1e1d61ea74a26727a348437
kopper42 <kopper42@gmail.com>
2025-06-21 13:50:16 -0600

    Install inotify-tools.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# install inotify-tools is required for file watching.
+apt install -y inotify-tools
 
 # copy the filewatcher.sh script to /usr/local/bin/
 cp filewatcher.sh /usr/local/bin/
